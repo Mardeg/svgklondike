cae6f16e17eb7807c230fe08d4013c20d4491182
Mardeg <Mardeg@users.noreply.github.com>
2026-06-18 12:04:13 +1000

    Giving it one last chance
    Add Content-Encoding and Cache-Control headers to response.

--- functions/deckspritesheet.svgz.js
+++ functions/deckspritesheet.svgz.js
@@ -1,10 +1,13 @@
 export async function onRequest(context) {
   const response = await context.env.ASSETS.fetch(context.request);
+  
   return new Response(response.body, {
+    status: response.status,
     headers: {
       "Content-Type": "image/svg+xml",
       "Content-Encoding": "gzip",
       "Cache-Control": "no-transform, no-cache"
-    }
+    },
+    encodeBody: "manual"
   });
 }
